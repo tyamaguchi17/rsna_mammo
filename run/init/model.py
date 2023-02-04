7de0c90c83463d2f3d87c42a89d2b5bdea417d38
tyamaguchi <charmq@preferred.jp>
2023-02-04 16:27:25 +0900

    add scripts

---
+++ run/init/model.py
@@ -0,0 +1,75 @@
+from logging import getLogger
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from omegaconf import DictConfig
+
+from src.nn.backbone import load_backbone
+from src.nn.backbones.base import BackboneBase
+from src.nn.heads.arc_face import ChannelWiseGeM, GeM
+from src.utils.checkpoint import get_weights_to_load
+
+logger = getLogger(__name__)
+
+
+def init_model_from_config(cfg: DictConfig, pretrained: bool):
+    model = nn.Sequential()
+    backbone = init_backbone(cfg, pretrained=pretrained)
+    forward_features = nn.Sequential()
+
+    forward_features.add_module("backbone", backbone)
+    if cfg.pool.type == "adaptive":
+        forward_features.add_module("pool", nn.AdaptiveAvgPool2d((1, 1)))
+        forward_features.add_module("flatten", nn.Flatten())
+    elif cfg.pool.type == "gem":
+        forward_features.add_module(
+            "pool", GeM(p=cfg.pool.p, p_trainable=cfg.pool.p_trainable)
+        )
+        forward_features.add_module("flatten", nn.Flatten())
+    elif cfg.pool.type == "gem_ch":
+        forward_features.add_module(
+            "pool",
+            ChannelWiseGeM(
+                dim=backbone.out_features,
+                p=cfg.pool.p,
+                requires_grad=cfg.pool.p_trainable,
+            ),
+        )
+        forward_features.add_module("flatten", nn.Flatten())
+
+    if cfg.use_bn:
+        forward_features.add_module("normalize", nn.BatchNorm1d(backbone.out_features))
+        forward_features.add_module("relu", torch.nn.PReLU())
+
+    model.add_module("forward_features", forward_features)
+    if cfg.head.type == "linear":
+        head = nn.Linear(backbone.out_features, cfg.output_dim, bias=True)
+    else:
+        raise ValueError(f"{cfg.head.type} is not implemented")
+    model.add_module("head", head)
+
+    if cfg.restore_path is not None:
+        logger.info(f'Loading weights from "{cfg.restore_path}"...')
+        ckpt = torch.load(cfg.restore_path, map_location="cpu")
+        model_dict = get_weights_to_load(model, ckpt)
+        model.load_state_dict(model_dict, strict=True)
+
+    return model
+
+
+def init_backbone(cfg: DictConfig, pretrained: bool) -> BackboneBase:
+    if cfg.use_encoder_2d or cfg.use_encoder_1d:
+        in_chans = cfg.encoder.out_chans
+    else:
+        in_chans = cfg.in_chans
+    backbone = load_backbone(
+        base_model=cfg.base_model,
+        pretrained=pretrained,
+        in_chans=in_chans,
+    )
+    if cfg.freeze_backbone:
+        for param in backbone.parameters():
+            param.requires_grad = False
+    return backbone
---
+++ run/init/preprocessing.py
@@ -0,0 +1,159 @@
+from typing import List, Optional
+
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+from omegaconf import DictConfig
+
+
+class Preprocessing:
+    def __init__(
+        self,
+        aug_cfg: Optional[DictConfig],
+        mean: List[float],
+        std: List[float],
+        h_resize_to: int,
+        w_resize_to: int,
+    ):
+        if aug_cfg is None:
+            self.aug_cfg = None
+        else:
+            self.aug_cfg = aug_cfg.copy()
+        self.mean = mean.copy()
+        self.std = std.copy()
+        self.h_resize_to = h_resize_to
+        self.w_resize_to = w_resize_to
+
+    def get_train_transform(self) -> A.Compose:
+
+        cfg = self.aug_cfg
+
+        if cfg.use_aug:
+            transforms = [
+                A.Resize(self.h_resize_to, self.w_resize_to),
+                A.ShiftScaleRotate(
+                    shift_limit_x=[-0.125, 0.125],
+                    shift_limit_y=[-0.10, 0.10],
+                    rotate_limit=15,
+                    p=0.3,
+                ),
+                A.Affine(
+                    rotate=(-cfg.rotate, cfg.rotate),
+                    translate_percent=(0.0, cfg.translate),
+                    shear=(-cfg.shear, cfg.shear),
+                    p=cfg.p_affine,
+                ),
+                A.RandomResizedCrop(
+                    self.h_resize_to,
+                    self.w_resize_to,
+                    scale=(cfg.crop_scale, 1.0),
+                    ratio=(cfg.crop_l, cfg.crop_r),
+                ),
+                A.ToGray(p=cfg.p_gray),
+                A.GaussianBlur(blur_limit=(3, 7), p=cfg.p_blur),
+                A.GaussNoise(p=cfg.p_noise),
+                A.Downscale(scale_min=0.5, scale_max=0.5, p=cfg.p_downscale),
+                A.RandomGridShuffle(grid=(2, 2), p=cfg.p_shuffle),
+                A.Posterize(p=cfg.p_posterize),
+                A.RandomBrightnessContrast(p=cfg.p_bright_contrast),
+                A.Cutout(p=cfg.p_cutout),
+                A.HorizontalFlip(p=0.5),
+                A.Normalize(mean=self.mean, std=self.std),
+                ToTensorV2(transpose_mask=True),
+            ]
+        if cfg.use_heavy_aug:
+            transforms = [
+                A.Resize(self.h_resize_to, self.w_resize_to),
+                A.Affine(
+                    rotate=(-cfg.rotate, cfg.rotate),
+                    translate_percent=(0.0, cfg.translate),
+                    shear=(-cfg.shear, cfg.shear),
+                    p=cfg.p_affine,
+                ),
+                A.RandomResizedCrop(
+                    self.h_resize_to,
+                    self.w_resize_to,
+                    scale=(cfg.crop_scale, 1.0),
+                    ratio=(cfg.crop_l, cfg.crop_r),
+                ),
+                # A.CLAHE(clip_limit=(1, 4), p=0.5),
+                A.OneOf(
+                    [
+                        A.OpticalDistortion(distort_limit=1.0),
+                        A.GridDistortion(num_steps=5, distort_limit=1.0),
+                        A.ElasticTransform(alpha=3),
+                    ],
+                    p=0.2,
+                ),
+                A.OneOf(
+                    [
+                        A.GaussNoise(var_limit=[10, 50]),
+                        A.GaussianBlur(),
+                        A.MotionBlur(),
+                        A.MedianBlur(),
+                    ],
+                    p=0.2,
+                ),
+                A.ToGray(p=cfg.p_gray),
+                A.GaussianBlur(blur_limit=(3, 7), p=cfg.p_blur),
+                A.GaussNoise(p=cfg.p_noise),
+                A.OneOf(
+                    [
+                        A.JpegCompression(),
+                        A.Downscale(scale_min=0.1, scale_max=0.15),
+                    ],
+                    p=0.2,
+                ),
+                # A.Downscale(scale_min=0.1, scale_max=0.15, p=0.2),
+                A.PiecewiseAffine(p=0.2),
+                A.Sharpen(p=0.2),
+                A.RandomGridShuffle(grid=(2, 2), p=cfg.p_shuffle),
+                A.Posterize(p=cfg.p_posterize),
+                A.RandomBrightnessContrast(p=cfg.p_bright_contrast),
+                A.Cutout(
+                    max_h_size=int(self.h_resize_to * 0.1),
+                    max_w_size=int(self.w_resize_to * 0.1),
+                    num_holes=5,
+                    p=0.5,
+                ),
+                A.HorizontalFlip(p=0.5),
+                A.Normalize(mean=self.mean, std=self.std),
+                ToTensorV2(transpose_mask=True),
+            ]
+        else:
+            transforms = [
+                # Targets: image, mask, bboxes, keypoints
+                A.Resize(self.h_resize_to, self.w_resize_to, p=1),
+                # Targets: image
+                # A.Normalize(mean=self.mean, std=self.std),
+                # Targets: image, mask
+                ToTensorV2(transpose_mask=True),
+            ]
+
+        aug = A.Compose(transforms)
+        return aug
+
+    def get_val_transform(self) -> A.Compose:
+        cfg = self.aug_cfg
+        transforms = [
+            # Targets: image, mask, bboxes, keypoints
+            A.Resize(self.h_resize_to, self.w_resize_to, p=1),
+            # Targets: image
+            A.Normalize(mean=self.mean, std=self.std),
+            # Targets: image, mask
+            ToTensorV2(transpose_mask=True),
+        ]
+        aug = A.Compose(transforms)
+        return aug
+
+    def get_test_transform(self) -> A.Compose:
+        cfg = self.aug_cfg
+        transforms = [
+            # Targets: image, mask, bboxes, keypoints
+            A.Resize(self.h_resize_to, self.w_resize_to, p=1),
+            # Targets: image
+            A.Normalize(mean=self.mean, std=self.std),
+            # Targets: image, mask
+            ToTensorV2(transpose_mask=True),
+        ]
+        aug = A.Compose(transforms)
+        return aug
