7f58e681aa59676185867e06c416a4623d6a710f
tyamaguchi17 <sunflower_2539@yahoo.co.jp>
2023-02-09 12:21:26 +0000

    rm elastic

--- run/init/preprocessing.py
+++ run/init/preprocessing.py
@@ -41,9 +41,9 @@ class Preprocessing:
                 A.RandomBrightnessContrast(0.3, 0.3, p=0.5),
                 A.OneOf(
                     [
-                        A.ElasticTransform(
-                            alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03
-                        ),
+#                        A.ElasticTransform(
+#                            alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03
+#                        ),
                         A.GridDistortion(),
                         A.OpticalDistortion(distort_limit=2, shift_limit=0.5),
                     ],
